b38fabde194bff946c4bd72d663bf42212eb1775
Sean Mangosing <seanmangosing@gmail.com>
2018-06-05 21:30:17 -0700

    updated

--- app.py
+++ app.py
@@ -2,8 +2,39 @@ import os
 from flask import Flask, render_template, request
 import stripe
 
+stripe_keys = {
+  'secret_key': os.environ.get('SECRET_KEY'),
+  'publishable_key': os.environ.get('PUBLISHABLE_KEY')
+}
+
+stripe.api_key = stripe_keys['secret_key']
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', key=stripe_keys['publishable_key'])
+
+@app.route('/charge', methods=['POST'])
+def charge():
+
+    print(request.form['stripeEmail'])
+
+    #Amount in cents
+    # amount = 500
+
+    # customer = stripe.Customer.create(
+    #     email='cusomter@example.com',
+    #     source=request.form['stripeToken']
+    # )
+
+    # charge = stripe.Charge.create(
+    #     customer=customer_id,
+    #     amount=amount,
+    #     currency='usd',
+    #     description='Flask Charge'
+    # )
+
+    #return render_template('charge.html', amount=amount)
+
+    return ''
